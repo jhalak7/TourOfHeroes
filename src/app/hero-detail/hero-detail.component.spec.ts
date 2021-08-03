50c3315c6ae660082deb3ac8b0103e4d39967dc5
Jhalak Chutani <37243673+jhalak7@users.noreply.github.com>
2021-08-03 09:20:21 +0530

    Update hero-detail.component.spec.ts

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { HeroService } from '../hero.service';
 
 import { HeroDetailComponent } from './hero-detail.component';
 const ActivatedRouteSpy = {
@@ -25,9 +26,12 @@ describe('HeroDetailComponent', () => {
      
     })
     .compileComponents();
+    
   });
 
   beforeEach(() => {
+    
+    const service = TestBed.inject(HeroService);
     fixture = TestBed.createComponent(HeroDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -36,4 +40,19 @@ describe('HeroDetailComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+ it('getHero',()=>{
+   spyOn(component,'getHero');
+   expect(component.getHero).toBeTruthy();
+   //expect(component.getHero).toHaveBeenCalled();
+ });
+ it('goBack',()=>{
+  spyOn(component,'goBack');
+  expect(component.goBack).toBeTruthy();
+  //expect(component.getHero).toHaveBeenCalled();
+});
+it('save',()=>{
+  spyOn(component,'save');
+  expect(component.save).toBeTruthy();
+  //expect(component.getHero).toHaveBeenCalled();
+});
 });
